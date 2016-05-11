443a4520674acc1dfa7dea22b44e3cf9cced91bf
Jacob Gray <jacob@jacobgray.me>
2016-05-11 12:28:05 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+ 
+
+def main():
+  setup_logging()
+
+#Yay, logging!
+ 
+def setup_logging():
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(logging.DEBUG)
+
+    # In addition to the basic stderr logging configured globally
+    # above, we'll use a log file for chatexchange.client.
+    wrapper_logger = logging.getLogger('ChatExchange.chatexchange.client')
+    wrapper_handler = logging.handlers.TimedRotatingFileHandler(
+        filename='client.log',
+        when='midnight', delay=True, utc=True, backupCount=7,
+    )
+    wrapper_handler.setFormatter(logging.Formatter(
+        "%(asctime)s: %(levelname)s: %(threadName)s: %(message)s"
+    ))
+    wrapper_logger.addHandler(wrapper_handler)
+
+if __name__ == '__main__':
+    main()
