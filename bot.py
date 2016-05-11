0569325bfed5a31c80ec1b8bf0806928bc972adf
Jacob Gray <jacob@jacobgray.me>
2016-05-11 12:22:28 -0400

    started on the bot

---
+++ bot.py
@@ -0,0 +1,17 @@
+
+import ChatExchange.chatexchange.client
+import ChatExchange.chatexchange.events
+import ChatExchange.chatexchange.browser
+
+class Bot(object):
+    
+    url = ""
+    rooms = []
+    
+    #Join a room
+    def join(id):
+      return url
+
+    # The class "constructor" - It's actually an initializer 
+    def __init__(self, url):
+        self.url = url
