693a634183eb2131d5ba11489542453872ed7b36
Jacob Gray <jacob@jacobgray.me>
2016-05-11 12:41:35 -0400

    oops

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 import ChatExchange.chatexchange.client
 
 #Login
-def user(host,email,p):
+def user(host, email, password):
   client = ChatExchange.chatexchange.client.Client(host)
   client.login(email, password)
   return client
