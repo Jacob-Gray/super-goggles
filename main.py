d9f0e87bb6a980d95e6d5a73d54d940cc6fcc355
Jacob Gray <jacob@jacobgray.me>
2016-05-11 18:25:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,8 @@ def on_message(message, client):
       os._exit(3)
     if command == "join":
       room_id = int(message.content.split()[2])
-      bot.join(client, room_id, on_message)
+      r = bot.join(client, room_id, on_message)
+      r.send_message("Hey guys, I joined by request of [@"+message.user.name+"](http://stackoverflow.com/users/"+str(message.user.id)+")")
       message.message.reply("I am now listening in room [`#"+str(room_id)+"`](http://chat.stackoverflow.com/rooms/"+str(room_id)+")")
     else:
       message.message.reply("`"+message.content+"` isn't a valid command.")
