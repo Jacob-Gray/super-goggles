91c70ad2ee3636de7590245f2d194e6510cfb498
Jacob Gray <jacob@jacobgray.me>
2016-05-11 20:44:49 -0400

    Update user.py

--- user.py
+++ user.py
@@ -19,6 +19,7 @@ def privileged(user_id):
 #sets user as privileged
 def setPrivileged(user_id):
   priv_users = open('privileged.json',"w+")
+  print priv_users
   users = json.load(priv_users)
   users[user_id] = True
   priv_users.write(json.dumps(users))
