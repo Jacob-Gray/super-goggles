adb0511712dad9fb60d0d6747455282a3a8521f3
Jacob Gray <jacob@jacobgray.me>
2016-06-01 15:43:44 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,12 @@ import ChatExchange.chatexchange.events
 rooms = {}
 
 def join(client, room_id, listen):
-  room = client.get_room(room_id)
-  room.join()
-  room.watch(listen)
-  rooms[room_id] = room
-  return room
+  if room_id not in rooms:
+    room = client.get_room(room_id)
+    room.join()
+    room.watch(listen)
+    rooms[room_id] = room
+    return room
 
 def leave(room_id):
   rooms[room_id].leave()
